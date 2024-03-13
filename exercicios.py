7535b96e33180a0952194aab038a71e221f48efe
GuiAntoniacomi <162726752+GuiAntoniacomi@users.noreply.github.com>
2024-03-13 17:31:35 -0300

    Correção

--- exercicios.py
+++ exercicios.py
@@ -75,6 +75,24 @@ n = int(input('Digite um número:'))
         print(f'{n} é primo')
     else:
         print(f'{n} não é primo')
+        
+# Resolução Guanabara:
+n = int(input('Digite um número:'))
+tot = 0
+for c in range(1, n+1):
+    if n % c == 0:
+        print('\033[33m', end=" ")
+        tot += 1
+    else:
+        print('\033[31m', end=" ")
+    print(f'{c}', end=" ")
+
+print(f'\n\033[mO número {n} foi divisível {tot} vezes.')
+
+if tot == 2:
+    print('É por isso que ele é primo')
+else:
+    print('É por isso que ele não é primo')
 
 # Agora utilizando For
 n = int(input('Digite um número:'))
