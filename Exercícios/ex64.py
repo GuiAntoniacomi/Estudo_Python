2b64cd6b991c4112b6377f6d9ba06f2cc1fd7902
GuiAntoniacomi <162726752+GuiAntoniacomi@users.noreply.github.com>
2024-03-15 18:01:05 -0300

    correçao

--- Exercícios/ex64.py
+++ Exercícios/ex64.py
@@ -1,8 +1,17 @@
-n = 0
+'''n = 0
 lista = []
 while n != 999:
     n = int(input('Digite um número: '))
     lista.append(n)
 x = len(lista) - 1
 y = sum(lista) - 999
-print(f'Foram digitados {x} valores e a soma deles é igual a {y}.')
+print(f'Foram digitados {x} valores e a soma deles é igual a {y}.')'''
+
+#Resolução Guanabara
+n = c = s = 0
+n = int(input('Digite um número [999 para parar]: '))
+while n != 999:
+    s += n
+    c += 1
+    n = int(input('Digite um número [999 para parar]: '))
+print(f'Foram digitados {c} valores e a soma deles é igual a {s}.')
--- Exercícios/ex65.py
+++ Exercícios/ex65.py
@@ -1,8 +1,10 @@
 from time import sleep
 n = int(input('Digite um número: '))
 lista = []
-lista.append(n)
 while True:
+    n = int(input('Digite um número: '))
+    lista = []
+    lista.append(n)
     print('Quer continuar adicionando números?\n[1] Sim\n[2] Não')
     x = int(input('Digite a opção escolhida:'))
     if x == 1:
@@ -19,4 +21,22 @@ maior = max(lista)
 menor = min(lista)
 print(f'O maior número digitado foi {maior:.0f} e o menor foi {menor:.0f} e a média dos números digitados é {media:.0f}.')
 
-        
+# Resolução Guanabara
+'''resp = "S"
+s = q = m = 0
+while resp in "Ss":
+    n = int(input('Digite um número: '))
+    s += n
+    q += 1
+    if q == 1: # Pois o primeiro numero digitado é tanto o maior quanto o menor
+        maior = menor = n
+    else:
+        if n > maior:
+            maior = n
+        if n < menor:
+            menor = n
+
+    resp = str(input('Quer Continuar? [S/N] ')).upper().strip()[0]
+m = s/q
+print(f'Você digitou {q} números e a média foi {m}.')     
+print(f'O maior valor foi {maior} e o menor foi {menor}.')'''
