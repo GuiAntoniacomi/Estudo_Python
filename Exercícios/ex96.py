1b72d983a538a70f8b77274441d3138bdec1d8be
GuiAntoniacomi <162726752+GuiAntoniacomi@users.noreply.github.com>
2024-04-02 17:08:38 -0300

    att

--- Exercícios/ex96.py
+++ Exercícios/ex96.py
@@ -2,6 +2,7 @@ def area(l, c):
     a = l * c
     print(f'A área de um terreno {l}x{c} é de {a}m².')
 
+
 print('Controle de Terrenos')
 print('----------------------')
 l = float(input('Largura (m): '))
