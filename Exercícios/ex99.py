772f6acd8b636c2ef246cb64e1db1d42a0e88354
GuiAntoniacomi <162726752+GuiAntoniacomi@users.noreply.github.com>
2024-04-02 17:38:21 -0300

    exercicios

--- Exercícios/ex99.py
+++ Exercícios/ex99.py
@@ -1,18 +1,24 @@
+#Faça um programa que tenha uma função chamada maior(), que receba vários parâmetros com valores inteiros. Seu programa tem que analisar todos os valores e dizer qual deles é o maior.
+from time import sleep
+
 def maior(*num):
+    cont = maior = 0
     print('=-'*30)
+    print('\nAnalisando os valores passados...')
     print('Foram informados {} valores ao todo.'.format(len(num)))
     print('Os valores informados foram: ', end='')
     for valor in num:
-        print(valor, end=' ')
-    print()
+        print(valor, end=' ', flush=True)
+        sleep(0.5)
+        if cont == 0:
+            maior = valor
+        else:
+            if valor > maior:
+                maior = valor
+        cont += 1
+    print(f'O maior valor informado foi {maior}.')
 
 
-numeros = []
-while True:
-    n = int(input('Digite um número: '))
-    numeros.append(n)
-    p = str(input('Deseja adicionar mais números? [S/N] ')).upper()[0]
-    if p == 'N':
-        break
-
-maior(*numeros)
+maior(2, 9, 4, 5, 7, 1)
+maior(4, 7, 0)
+maior(1, 2)
