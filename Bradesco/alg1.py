abe4e0785203ceae44ee160eef78a93e392bcaaf
GuiAntoniacomi <162726752+GuiAntoniacomi@users.noreply.github.com>
2024-04-22 17:01:04 -0300

    att

---
+++ Bradesco/alg1.py
@@ -0,0 +1,7 @@
+perg = int(input('Está sol?\n1. Sim\n2. Não\n'))
+if perg == 1:
+    print('Ótimo dia para passear no parque.')
+elif perg == 2:
+    print('Ótimo dia para ficar em casa maratonando séries.')
+else:
+    print('Escolha uma opção valida.')
