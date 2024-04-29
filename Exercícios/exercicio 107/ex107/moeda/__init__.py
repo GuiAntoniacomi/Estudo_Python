c4f775e3c672dfd4016d761dad89ce30690c8ab1
GuiAntoniacomi <162726752+GuiAntoniacomi@users.noreply.github.com>
2024-04-29 17:52:59 -0300

    ex modulo

---
+++ Exercícios/exercicio 107/ex107/moeda/__init__.py
@@ -0,0 +1,11 @@
+def metade(n):
+    return n / 2
+
+def dobro(n):
+    return n * 2
+
+def aumentar(n, t=10):
+    return n + (n * t / 100)
+
+def diminuir(n, t=13): 
+    return n - (n * t / 100)   
