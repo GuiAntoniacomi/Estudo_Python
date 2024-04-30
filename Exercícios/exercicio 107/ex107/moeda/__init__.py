42524b0fd9f8c100b8ac4054f03b2446afa84ec1
GuiAntoniacomi <162726752+GuiAntoniacomi@users.noreply.github.com>
2024-04-30 16:47:31 -0300

    estudos

--- Exercícios/exercicio 107/ex107/moeda/__init__.py
+++ Exercícios/exercicio 107/ex107/moeda/__init__.py
@@ -1,11 +1,38 @@
-def metade(n):
-    return n / 2
+def metade(n, sit=False):
+    if sit:
+        return moeda(n / 2)
+    else:
+        return n / 2
 
-def dobro(n):
-    return n * 2
+def dobro(n, sit=False):
+    if sit:
+        return moeda(n * 2)
+    else:
+        return n * 2
 
-def aumentar(n, t=10):
-    return n + (n * t / 100)
+def aumentar(n, a=10, sit=False):
+    if sit:
+        return moeda(n + (n * a / 100))
+    else:
+        return n + (n * a / 100)
 
-def diminuir(n, t=13): 
-    return n - (n * t / 100)   
+def diminuir(n, r=13, sit=False): 
+    if sit:
+        return moeda(n - (n * r / 100))
+    else:
+        return n - (n * r / 100)
+
+def moeda(n):
+    return f'R${n:.2f}'.replace('.', ',')
+
+def resumo(preço=0, taxaa=10, taxab=5):
+    print('-' * 30)
+    print("RESUMO DO VALOR".center(30))
+    print('-' * 30)
+    print(f'Preço analisad: \t{moeda(preço)}')
+    print(f'Dobro do preço: \t{dobro(preço, True)}')
+    print(f'Metade do preço: \t{metade(preço, True)}')
+    print(f'Com {taxaa}% de aumento: \t{aumentar(preço, taxaa, True)}')
+    print(f'Com {taxab}% de redução: \t{diminuir(preço, taxab, True)}')
+    print('-' * 30)
+ 
--- Exercícios/exercicio 107/program.py
+++ Exercícios/exercicio 107/program.py
@@ -1,7 +1,4 @@
-from ex107 import moeda
+from ex107 import moeda as mo
 
 p= float(input('Digite o preço: R$ '))
-print(f'A metade de {p} é {moeda.metade(p)}')
-print(f'O dobro de {p} é {moeda.dobro(p)}')
-print(f'Aumentando 10%, temos {moeda.aumentar(p, 10)}')
-print(f'Reduzindo 13%, temos {moeda.diminuir(p, 13)}')
+mo.resumo(p)
